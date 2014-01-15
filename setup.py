984f269f2c8ef676e2ad26e2444349554642bdad
nmohr <niklas.mohr@cern.ch>
2014-01-15 16:01:18 +0100

    Remove

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
         Extension("dtb",
-                  ["python/dtb.pyx","src/analyzer.cpp","src/protocol.cpp","src/datastream.cpp","src/pixel_dtb.cpp","src/rpc.cpp","src/rpc_error.cpp","src/profiler.cpp","src/rpc_calls.cpp","src/rpc_io.cpp","src/settings.cpp", "src/linux/rs232.cpp","src/USBInterface.libftd2xx.cc"],
-                  include_dirs=["src","src/linux","/usr/local/include"],
+                  ["python/dtb.pyx","src/analyzer.cpp","src/protocol.cpp","src/datastream.cpp","src/pixel_dtb.cpp","src/rpc.cpp","src/rpc_error.cpp","src/profiler.cpp","src/rpc_calls.cpp","src/rpc_io.cpp","src/settings.cpp","src/USBInterface.libftd2xx.cc"],
+                  include_dirs=["src","/usr/local/include"],
                   language="c++",
                   libraries=["m"] 
         ),
