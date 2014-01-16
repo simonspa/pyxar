2b54468646b7bdad49afaff442eea5cd96074e76
nmohr <niklas.mohr@cern.ch>
2014-01-16 17:22:52 +0100

    Add phmap

---
+++ python/phmap.py
@@ -0,0 +1,9 @@
+import test
+
+class PHMap(test.Test):
+
+    def prepare(self, config):
+        self.n_triggers = int(config.get('PHMap','n_triggers'))
+
+    def run(self, config):
+        self.tb.get_ph(self.n_triggers)
