bdb307ba7f7377be66279fdf8b60dd62dff875f3
philippeller <peller@phys.ethz.ch>
2014-01-16 17:21:20 +0100

    full module decode

--- python/helpers.py
+++ python/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy
 
 def list_to_grid(n_cols,n_rows,a_list):
     """Helper function that converts a list from TB into a grid with rows and cols"""
@@ -27,3 +28,23 @@ def decode(n_cols, n_rows, address, a_list):
             row = 0
         roc_data[col][row] += a_list[i]
     return roc_data
+
+def decode_full(n_rocs, n_cols, n_rows, address, a_list):
+    s = (n_cols,n_rows)
+    datas = []
+    for i in range(n_rocs):
+        datas.append(np.zeros(s))
+    for i, adr in enumerate(address):
+        row = adr & 0xff
+        col = (adr >> 8) & 0xff
+        roc = (adr >> 16) & 0xff
+        tbm = (adr >> 24) & 0xff
+        roc += tbm*8
+        if ( (col >= n_cols or col < 0) or (row >= n_rows or row < 0) or (roc < 0 or roc >= n_rocs)):
+            #raise Exception("Adress: %s decoded (col,row) (%s,%s)"%(adr,col,row))
+            print "Adress: %s decoded (col,row) (%s,%s)"%(hex(adr),col,row)
+            col = 0
+            row = 0
+        else:
+            datas[roc][col][row] += a_list[i]
+    return datas
