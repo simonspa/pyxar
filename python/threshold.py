446c3349b91c1a8012aa665ccf70bc54c6c33b9a
jhoss <hoss@phys.ethz.ch>
2014-08-20 10:20:22 +0200

    activated signal probes on DTB

--- python/threshold.py
+++ python/threshold.py
@@ -11,4 +11,6 @@ class Threshold(test.Test):
 
     def run(self, config): 
         self.logger.info('Running threshold test')
+        self.tb.SignalProbe("a1","ctr")
+        self.tb.SignalProbe("a2","clk")
         dut_thr_map = self.tb.get_threshold(self.n_triggers, self.dac, self.xtalk, self.cals, self.reverse) 
