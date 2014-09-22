606216271d169e353cacfd43617a14b25b64a515
jhoss <hoss@phys.ethz.ch>
2014-09-22 09:52:32 +0200

    cleanup

--- python/threshold.py
+++ python/threshold.py
@@ -7,10 +7,8 @@ class Threshold(test.Test):
         self.xtalk = int(eval(config.get('Threshold','xtalk')))
         self.dac = config.get('Threshold','dac')
         self.n_triggers = int(config.get('Threshold','n_triggers'))
-        self.reverse = eval(config.get('Threshold','reverse'))
+        self.reverse = int(eval(config.get('Threshold','reverse')))
 
     def run(self, config): 
         self.logger.info('Running threshold test')
-        self.tb.SignalProbe("a1","ctr")
-        self.tb.SignalProbe("a2","clk")
         dut_thr_map = self.tb.get_threshold(self.n_triggers, self.dac, self.xtalk, self.cals, self.reverse) 
