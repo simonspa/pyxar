4be6c45e18c99c6a3be5a40891629836b851c423
jhoss <hoss@phys.ethz.ch>
2014-10-13 17:06:02 +0200

    corrected timewalk test

--- python/timewalk.py
+++ python/timewalk.py
@@ -8,22 +8,19 @@ class Timewalk(test.Test):
     def prepare(self, config):
         self.n_triggers = int(config.get('Timewalk','n_triggers'))
         self.caldel_margin = 10
-        self.vcal_margin = 0
+        self.vcal_margin = 5
         self.caldel1 = []
         self.caldel2 = []
         self.caldel4 = []
         self.caldel_width = []
         self.delta_caldel = []
         self.timewalk = []
-        self.vcal_min = []
-        self.failed_vcal_threshold = 0
         for roc in self.dut.rocs():
             self.caldel1.append([])
             self.caldel2.append([])
             self.caldel4.append([])
             self.delta_caldel.append([])
             self.timewalk.append([])
-            self.vcal_min.append([])
             self.caldel_width.append([])
 
     def run(self, config):
@@ -32,9 +29,8 @@ class Timewalk(test.Test):
         self.tb.testAllPixels(False)
         for roc in self.dut.rocs():
             self.logger.info('Measuring %s' %roc)
-            count = 0
-            for col in range(52):
-                for row in range(40):
+            for col in range(7):
+                for row in range(7):
                     #Enabling pixel under test
                     roc.pixel(col,row).active = True
                     #set Vcal 255 high range to measure efficiency window width
@@ -50,26 +46,22 @@ class Timewalk(test.Test):
                     delta_thr = thr2 - thr1
                     self.caldel_width[roc.number].append(delta_thr)
                     conversion = delta_thr / 25
-                    #reset Vcal and CtrlReg
-                    self.tb.set_dac_roc(roc, 'Vcal', 200)
+                    #reset CtrlReg
                     self.tb.set_dac_roc(roc, 'CtrlReg', 0)
-
-                    #find lowest Vcal value above threshold
-                    self.tb.set_dac_roc(roc, 'CalDel', thr1 + self.caldel_margin)
-                    thr3 = self.tb.get_pixel_threshold(roc, col, row, self.n_triggers, 'Vcal', 90, False, False, False)
-                    if thr3 > 254:
-                        self.failed_vcal_threshold += 1
-                        continue
-                    self.logger.debug("Vcal threshold: %i" %thr3)
-                    self.vcal_min[roc.number].append(thr3)
-                    #set Vcal_min
-                    self.tb.set_dac_roc(roc, 'Vcal', thr3 + self.vcal_margin)
-                    self.tb.set_dac_roc(roc, 'CtrlReg', 0)
-                    #find rising edge of efficiency window for Vcal_min
-                    thr4 = self.tb.get_pixel_threshold(roc, col, row, self.n_triggers, 'CalDel', 50, False, False, False)
-                    self.caldel4[roc.number].append(thr4)
+                    #find the smalles CalDel value for which 50% of the triggers                     #can be seen at any Vcal value
+                    for i in range(50):
+                        if thr1-50 > 0:
+                            caldel = thr1 - 50 + i
+                        else:
+                            caldel = i
+                        self.tb.set_dac_roc(roc, 'CalDel', caldel)
+                        thr3 = self.tb.get_pixel_threshold(roc, col, row, self.n_triggers, 'Vcal', 50, False, False, False)
+                        if (thr3 != 255 and thr3 != 0): 
+                            break
+                    #append found caldel value
+                    self.caldel4[roc.number].append(caldel)
                     #calculate timewalk
-                    timewalk_caldel = thr1 - thr4
+                    timewalk_caldel = thr1 - caldel
                     self.delta_caldel[roc.number].append(timewalk_caldel)
                     timewalk_time = timewalk_caldel / conversion
                     self.timewalk[roc.number].append(timewalk_time)
@@ -79,23 +71,18 @@ class Timewalk(test.Test):
                         self.logger.debug('caldel1      = %i' %thr1)
                         self.logger.debug('caldel2      = %i' %thr2)
                         self.logger.debug('caldel width = %i' %delta_thr)
-                        self.logger.debug('vcal_min     = %i' %thr3)
-                        self.logger.debug('caldel4      = %i' %thr4)
+                        self.logger.debug('caldel4      = %i' %caldel)
                         self.logger.debug('timewalk dac = %i' %timewalk_caldel)
                         self.logger.debug('timewalk ns  = %i' %timewalk_time)
-                    count += 1
-                    self.logger.debug("measured %i pixels" %count)
         #convert to numpy arrays
         self.array1=numpy.array(self.caldel1)
         self.array2=numpy.array(self.caldel2)
         self.array4=numpy.array(self.caldel4)
         self.array5=numpy.array(self.delta_caldel)
-        self.array6=numpy.array(self.vcal_min)
         self.array7=numpy.array(self.timewalk)
         self.array8=numpy.array(self.caldel_width)
         #disable all pixels
         self.tb.testAllPixels(False)
-        self.logger.info("Could not find Vcal thhreshold for %i pixels" %self.failed_vcal_threshold)
                     
         
 
@@ -108,8 +95,6 @@ class Timewalk(test.Test):
             self._histos.append(caldel2_histo)
             caldel_width_histo = Plotter.create_th1(self.array8[roc.number],'CalDel_width_distribution_%s' %roc, 'CalDel', '# pixels',1,255)
             self._histos.append(caldel_width_histo)
-            vcal_min_histo = Plotter.create_th1(self.array6[roc.number],'Vcal_min_distribution_%s' %roc, 'Vcal', '# pixels',1,255)
-            self._histos.append(vcal_min_histo)
             caldel4_histo = Plotter.create_th1(self.array4[roc.number],'CalDel4_distribution_%s' %roc, 'CalDel', '# pixels',1,255)
             self._histos.append(caldel4_histo)
             timewalk_caldel_histo = Plotter.create_th1(self.array5[roc.number],'Timewalk_CalDel_units_distribution_%s' %roc, 'CalDel', '# pixels',1,255)
